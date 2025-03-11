3fa2a9fab624ae9190604d430631632b0500f145
Luka9905 <150483584+Luka9905@users.noreply.github.com>
2025-03-11 09:15:38 +0300

    Update log.c

--- src/log.c
+++ src/log.c
@@ -4,12 +4,7 @@
 #include "log.h"
 
 void log_to_file(const char *path) {
-    if (access(path, W_OK) == -1) {
-        FILE *file = fopen(path, "w");
-        fclose(file);
-    }
-
-    FILE *file = freopen(path, "a", stdout);
+    FILE *file = freopen(path, "w", stdout);
     if (file == NULL) {
         perror("freopen");
         exit(EXIT_FAILURE);
