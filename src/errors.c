95554609ba237b53a69f616db6f2c7a771ad53da
Luka9905 <150483584+Luka9905@users.noreply.github.com>
2025-03-11 09:15:00 +0300

    Update errors.c

--- src/errors.c
+++ src/errors.c
@@ -4,12 +4,7 @@
 #include "errors.h"
 
 void errors_to_file(const char *path) {
-    if (access(path, W_OK) == -1) {
-        FILE *file = fopen(path, "w");
-        fclose(file);
-    }
-
-    FILE *file = freopen(path, "a", stderr);
+    FILE *file = freopen(path, "w", stderr);
     if (file == NULL) {
         perror("freopen");
         exit(EXIT_FAILURE);
